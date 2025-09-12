7a6b4a21011a38c4ba4a6fcb6a30e814670dab42
Anderson <negaobarcellos@gmail.com>
2025-09-12 17:21:44 -0300

    Add files via upload

---
+++ olamundonovo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Olá Mundo Novo!\n");
+    printf("Tem alguem ai?\n");
+    printf("Alooouuuuuu!\n");
+    printf("Responda se estiver ai!\n");
+
+    return 0;
+
+
+}
